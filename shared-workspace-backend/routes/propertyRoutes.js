3d298959fe9a53e7726e6764c59c7a794ab26678
paulineguiao <allyguiao@gmail.com>
2026-06-23 18:48:32 -0600

    Update propertyRoutes.js

--- shared-workspace-backend/routes/propertyRoutes.js
+++ shared-workspace-backend/routes/propertyRoutes.js
@@ -2,21 +2,27 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/authMiddleware");
+const upload = require("../middleware/uploadMiddleware");
+
 const {
     createProperty,
     getAllProperties,
     updateProperty,
-    deleteProperty
+    deleteProperty,
+    getPropertyById
 } = require("../controllers/propertyController");
 
 // CREATE property (owner only)
-router.post("/", auth, createProperty);
+router.post("/", auth, upload.single("image"), createProperty);
 
 // GET all properties
 router.get("/", getAllProperties);
 
+// GET property by ID
+router.get("/:id", auth, getPropertyById);
+
 // UPDATE property (owner only)
-router.put("/:id", auth, updateProperty);
+router.put("/:id", auth, upload.single("image"), updateProperty);
 
 // DELETE property (owner only)
 router.delete("/:id", auth, deleteProperty);
