bb14d8a74dd30f4d7f53efb14e1865db871a21f7
paulineguiao <allyguiao@gmail.com>
2026-06-23 18:49:15 -0600

    Update workspaceRoutes.js

--- shared-workspace-backend/routes/workspaceRoutes.js
+++ shared-workspace-backend/routes/workspaceRoutes.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/authMiddleware");
+const upload = require("../middleware/uploadMiddleware");
+
 const {
     createWorkspace,
     getAllWorkspaces,
@@ -9,7 +11,6 @@ const {
     getWorkspaceById,
     updateWorkspace,
     deleteWorkspace,
-    addRating,
     getRatings,
     addReview,
     getReviews
@@ -18,26 +19,26 @@ const {
 // SEARCH workspaces (coworker)
 router.get("/search", searchWorkspaces);
 
-// GET workspace details (coworker)
-router.get("/:id", getWorkspaceById);
-
 // GET all workspaces
 router.get("/", getAllWorkspaces);
 
+// IMPORTANT: longer routes FIRST
+router.get("/:id/ratings", getRatings);
+router.get("/:id/reviews", getReviews);
+
+router.post("/:id/review", auth, addReview);
+
+// GET workspace details (coworker)
+router.get("/:id", getWorkspaceById);
+
 // CREATE workspace (owner only)
-router.post("/", auth, createWorkspace);
+router.post("/", auth, upload.single("image"), createWorkspace);
 
 // UPDATE workspace (owner only)
-router.put("/:id", auth, updateWorkspace);
+router.put("/:id", auth, upload.single("image"), updateWorkspace);
 
 // DELETE workspace (owner only)
 router.delete("/:id", auth, deleteWorkspace);
-
-router.post("/:id/rate", auth, addRating);
-router.get("/:id/ratings", getRatings);
-
-router.post("/:id/review", auth, addReview);
-router.get("/:id/reviews", getReviews);
 
 
 module.exports = router;
